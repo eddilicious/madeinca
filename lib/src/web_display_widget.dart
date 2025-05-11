7c07e5af88f93ca870e38051c111cc1a678a81db
eddilicious <88624281+eddilicious@users.noreply.github.com>
2025-05-11 16:17:51 -0400

    fixed onLoadStop

--- lib/src/web_display_widget.dart
+++ lib/src/web_display_widget.dart
@@ -15,7 +15,6 @@ class WebDisplayWidget extends StatefulWidget {
 }
 
 class _WebDisplayWidgetState extends State<WebDisplayWidget> {
-  final String jsonData = widget.jsonData;
   bool linkClicked = false; // Track if a link has been clicked
 
 //final String jsonData = '{  "code": "OK",  "total": 1,  "offset": 0,  "items": [{  "ean": "0064642051967",  "title": "Jamieson Vitamin C + D Chewable  75 tabs  for ASIN  B00CP7Q59Q",  "description": "Jamieson Vitamins C & D - Chewable Morello Cherry is the first Vitamin C formula to be enriched with Vitamin D3  the same disease-fighting form produced when your skin is exposed to sunlight. Combined  they protect you - even during the winter months when sunshine is limited. This innovative product is a great choice for anyone who regularly takes Vitamin C  but wants additional protection from Vitamin D.",  "upc": "064642051967",  "brand": "Jamieson",  "model": "5196",  "color": "",  "size": "",  "dimension": "",  "weight": "",  "category": "Sporting Goods > Outdoor Recreation > Equestrian > Horse Care",  "currency": "",  "lowest_recorded_price": 5.77,  "highest_recorded_price": 81.88,  "images": [  "https://i5.walmartimages.com/asr/c5d38cc3-40fd-4a10-8ab6-185c1b82205a.42c3740d6d743f562e425b1fe53208bc.jpeg?odnHeight=450&odnWidth=450&odnBg=ffffff",  "https://i5.walmartimages.ca/images/Large/921/794/6000196921794.jpg"  ],  "offers": [{  "merchant": "Wal-Mart.com",  "domain": "walmart.com",  "title": "Jamieson Vitamin C + D Chewable  75 tabs  for ASIN  B00CP7Q59Q",  "currency": "",  "list_price": "",  "price": 21.32,  "shipping": "Free Shipping",  "condition": "New",  "availability": "Out of Stock",  "link": "https://www.upcitemdb.com/norob/alink/?id=13q213v2z213c474q2&tid=1&seq=1739312069&plt=99950b8f3ed7bf6584bf588f492391cb",  "updated_t": 1683311707  }, {  "merchant": "WalMart Canada",  "domain": "walmart.ca",  "title": "Jamieson Laboratories Jamieson Chewable Vitamin C 500 Mg And Vitamin D3 500 Iu Morello Cherry Flavour Tablets",  "currency": "CAD",  "list_price": 6.97,  "price": 5.77,  "shipping": "",  "condition": "New",  "availability": "",  "link": "https://www.upcitemdb.com/norob/alink/?id=v2r213v2036374d4q2&tid=1&seq=1739312069&plt=50a2100415cebfa5455b1bc5972ddcd4",  "updated_t": 1624235456  }],  "elid": 285156567729  }]  }';
@@ -60,7 +59,7 @@ class _WebDisplayWidgetState extends State<WebDisplayWidget> {
           onLoadStop: (controller,url) async {
             if (linkClicked) return;
             
-            final jsonDataString = jsonEncode(jsonData); // Convert JSON data to string
+            final jsonDataString = jsonEncode(widget.jsonData); // Convert JSON data to string
 
               // Show a Flutter AlertDialog
   await showDialog(
